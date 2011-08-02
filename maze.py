e07f1d1aabf90c79bc4eda3493fef79345e60def
EkkiD <erick.dransch@gmail.com>
2011-08-01 22:35:48 -0400

    logic for DFS added, not functioning correctly yet

--- maze.py
+++ maze.py
@@ -1,4 +1,6 @@
 import sys, pygame
+from collections import deque
+import random
 
 pygame.init()
 
@@ -11,6 +13,8 @@ size = width, height = (2*base_offset)+(grid_cols*square_pixels), (2*base_offset
 black = 0,0,0
 white = 255,255,255
 grey  = 0x6E6D6Da
+green = 0x008000
+    
 
 class maze:
     """This class handles all of the maze generation and solving functions.
@@ -30,25 +34,27 @@ class maze:
     east = 2
     south = 4
     west = 8
-    
-    nodes = [None] * grid_rows
-    for i in range(grid_rows):
-        nodes[i] = [None] * grid_cols
-
+       
     screen = pygame.display.set_mode(size)
     
     clock = pygame.time.Clock()
     
     def __init__(self):
-        
+        self.nodes = [None] * grid_rows
+        for i in range(grid_rows):
+            self.nodes[i] = [None] * grid_cols
+
         # initialize the array as a valid grid.
         for i in range(grid_rows):
             for j in range(grid_cols):
-                maze.nodes[i][j] = maze.north | maze.east | maze.south | maze.west
-                if j == 0:               maze.nodes[i][j] = maze.nodes[i][j] ^ maze.west
-                if j == grid_cols - 1:   maze.nodes[i][j] = maze.nodes[i][j] ^ maze.east
-                if i == 0:               maze.nodes[i][j] = maze.nodes[i][j] ^ maze.north
-                if i == grid_rows - 1:   maze.nodes[i][j] = maze.nodes[i][j] ^ maze.south
+                self.nodes[i][j] = self.north | self.east | self.south | self.west
+                if j == 0:               self.nodes[i][j] = self.nodes[i][j] ^ self.west
+                if j == grid_cols - 1:   self.nodes[i][j] = self.nodes[i][j] ^ self.east
+                if i == 0:               self.nodes[i][j] = self.nodes[i][j] ^ self.north
+                if i == grid_rows - 1:   self.nodes[i][j] = self.nodes[i][j] ^ self.south
+
+        
+        self.cellStack = deque()
 
 
     def run(self):
@@ -57,14 +63,14 @@ class maze:
                 if event.type == pygame.QUIT: sys.exit()
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_RIGHT:
-                        maze.DoIteration();
+                        self.DoIteration();
     
-            maze.screen.fill(black)
-            maze.DrawScreen(self, maze.screen)
-            maze.clock.tick(40)
+            self.screen.fill(black)
+            self.DrawScreen(maze.screen)
+            self.clock.tick(40)
             
     def DoIteration(self):
-        global grey
+        self.DFSGenerate()     
     
     
     def DrawScreen(self, screen): 
@@ -78,17 +84,106 @@ class maze:
             for col in range(grid_cols):
                 off_x = base_offset + col * square_pixels
                 off_y = base_offset + row * square_pixels
-                if maze.nodes[row][col] & maze.north:
+                if self.nodes[row][col] & self.north:
                     assert row > 0, "Can't draw north of row 0"
-                    assert maze.nodes[row-1][col] & maze.south, "Node %d, %d should have 'south' set"  % (row-1, col)
+                    assert self.nodes[row-1][col] & self.south, "Node %d, %d should have 'south' set"  % (row-1, col)
                     pygame.draw.line(screen, grey, (off_x+2, off_y), (off_x+square_pixels-2, off_y), 2)
-                if maze.nodes[row][col] & maze.west:
+                if self.nodes[row][col] & self.west:
                     assert col > 0, "Can't draw west of col 0"
-                    assert maze.nodes[row][col-1] & maze.east, "Node %d, %d should have 'east' set"  % (row, col-1)
+                    assert self.nodes[row][col-1] & self.east, "Node %d, %d should have 'east' set"  % (row, col-1)
                     pygame.draw.line(screen, grey, (off_x, off_y+2), (off_x, off_y+square_pixels-2), 2)
+
+        for node in self.cellStack:
+            col = node[0]
+            row = node[1]
+            off_x = base_offset + col * square_pixels
+            off_y = base_offset + row * square_pixels
+            rect = (off_x+2, off_y+2, off_x + square_pixels - 2, off_y + square_pixels - 2)
+            pygame.draw.rect(screen, green, rect)
+
     
         pygame.display.flip()
-    
+
+
+    def DFSGenerate(self):
+        neighbors = []
+        try:
+            while True:  
+                top = self.cellStack.pop()
+                row = top[0] 
+                col = top[1]
+                if (nodes[row][col] & self.north) and self.CheckCellForWalls(row-1, col):
+                    neighbors.append(self.north)
+
+                if (nodes[row][col] & self.south) and self.CheckCellForWalls(row+1, col):
+                    neighbors.append(self.south)
+
+                if (nodes[row][col] & self.west) and self.CheckCellForWalls(row, col-1):
+                    neighbors.append(self.west)
+
+                if (nodes[row][col] & self.east) and self.CheckCellForWalls(row, col+1):
+                    neighbors.append(self.east)
+
+                if(neighbors.__len__() == 0):
+                    continue
+
+                index = randint(0,neighbors.__len__()-1)
+                direction = neighbors[index]
+                if(direction & self.north):
+                    assert nodes[row-1][col] & self.south, "Node %d, %d should have 'south' set"  % (row-1, col)
+                    nodes[row-1][col] ^ self.south
+                    nodes[row][col] ^ self.northa
+                    new_loc = (row-1, col)
+
+                elif(direction & self.south):
+                    assert nodes[row+1][col] & self.north, "Node %d, %d should have 'north' set"  % (row+1, col)
+                    nodes[row+1][col] ^ self.north
+                    nodes[row][col] ^ self.south
+                    new_loc = (row+1, col)
+
+                elif(direction & self.east):
+                    assert nodes[row][col+1] & self.west, "Node %d, %d should have 'east' set"  % (row, col+1)
+                    nodes[row][col+1] ^ self.west
+                    nodes[row][col] ^ self.east
+                    new_loc = (row, col+1)
+
+                elif(direction & self.west):
+                    assert nodes[row][col-1] & self.east, "Node %d, %d should have 'south' set"  % (rows, col+1)
+                    nodes[row][col-1] ^ self.east
+                    nodes[row][col] ^ self.west
+                    new_loc = (row, col-1)
+
+                cellStack.push(top)
+                cellStack.push(new_loc)
+                return
+
+        except IndexError:
+            return
+            
+
+
+    #TODO: Make a separate class for nodes, and add this function to that class.
+    def CheckCellForWalls(self, row, col):
+        """ Return true if the node has all Walls up, 
+            false otherwise """
+
+        if  ((row < 0) or (row >= grid_rows)):
+                return False
+        if ((col < 0) and (col >= grid_col)):
+                return False
+
+        this_node = self.nodes[row][col]
+
+        if ((row != 0) and not(this_node & self.west)):
+            return False
+        if ((row != grid_rows-1) and not(this_node & self.east)):
+            return False
+        if ((col != 0) and not(this_node & self.north)):
+            return False
+        if ((col != grid_cols-1) and not(this_node & self.south)):
+            return False
+
+        return True
 
 
 if __name__ == "__main__":
